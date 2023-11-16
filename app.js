ad444e7a740fc7c67edb8b1b84372d33f6c1166a
ahmedmehmood5147 <am9474036@gmail.com>
2023-11-16 17:43:45 +0500

    Java-assignment5

---
+++ app.js
@@ -0,0 +1,90 @@
+var num1 = 3;
+var num2 = 5;
+var num3 = (num1 + num2)
+document.write ("Sum of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + num3)
+var num1 = 3;
+var num2 = 5;
+var num3 = (num1 - num2)
+document.write ("<br> Sub of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + num3)
+var num1 = 3;
+var num2 = 5;
+var num3 = (num1 * num2)
+document.write ("<br> Multiplication of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + num3)
+var num1 = 3;
+var num2 = 5;
+var num3 = (num1 / num2)
+document.write ("<br>Division of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + num3)
+var num1 = 9;
+var num2 = 3;
+var num3 = (num1 % num2)
+document.write ("<br>Modulu of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + num3)
+var num2 = 5;
+document.write ("<br>initial value 5")
+var num3 = 5 + 1;
+document.write ("<br>value after increment is" + " " + num3)
+var num4 = num3 + 7;
+document.write ("<br>value after addition is" + " " + num4)
+var num5 = num4 - 1;
+document.write ("<br>value after decrement is" + " " + num5)
+var num6 = num5 % 3;
+document.write ("<br>The reminder is" + " " + num6)
+var movie_ticket = 600;
+var buyingTicket = "5";
+var quantity = movie_ticket * buyingTicket
+document.write ("<br>Total cost to buy" + " " + buyingTicket + " " + "to a movie is" + " " + quantity)
+var table = 4
+document.write ("<br>" +table + " " + "x" + " " + "1" + "=" + table*1 )
+document.write ("<br>" +table + " " + "x" + " " + "1" + "=" + table*2 )
+document.write ("<br>" +table + " " + "x" + " " + "1" + "=" + table*3 )
+document.write ("<br>" +table + " " + "x" + " " + "1" + "=" + table*4 )
+document.write ("<br>" +table + " " + "x" + " " + "1" + "=" + table*5 )
+document.write ("<br>" +table + " " + "x" + " " + "1" + "=" + table*6 )
+document.write ("<br>" +table + " " + "x" + " " + "1" + "=" + table*7 )
+document.write ("<br>" +table + " " + "x" + " " + "1" + "=" + table*8 )
+document.write ("<br>" +table + " " + "x" + " " + "1" + "=" + table*9 )
+document.write ("<br>" +table + " " + "x" + " " + "1" + "=" + table*10 )
+document.write ("<br>" + "Shopping Cart")
+var item1 = 650;
+var item2 = 100;
+var quantityOfItem1 = 3;
+var quantityOfItem2 = 7;
+var shippingCharges = 100 
+document.write ("<br>"+"Price of item 1 is" + " " + item1)
+document.write ("<br>"+ "Quantity of item 1 is" + " " + quantityOfItem1)
+document.write ("<br>"+"Price of item 2 is" + " " + item2)
+document.write ("<br>"+ "Quantity of item 2 is" + " " + quantityOfItem2)
+document.write ("<br>" + "shipping charges" + " " + shippingCharges)
+var order = item1 * quantityOfItem1 + item2 * quantityOfItem2 + shippingCharges;
+document.write ("<br>" + "Total cost of your order is"+ " " + order)
+var totalMarks = 980;
+var marksObtained = 894;
+var percentage = marksObtained / totalMarks * 100
+document.write ("MARKSHEETS")
+document.write ("<br>" + "Total Marks:" + " " + totalMarks)
+document.write ("<br>" + "Marks obtained:" + " " + marksObtained)
+document.write ("<br>" + "percentage:" + " " + percentage) 
+document.write ("<br>" + "Currency in PKR")
+var usDollars = 10 
+var PkRupees = 104;
+var Currency = usDollars * PkRupees
+var saudiRiyals = 25;
+var PkRupees1 = 28;
+var Currency1 = saudiRiyals * PkRupees1
+var pkr = Currency + Currency1
+document.write ("<br>" + "Total currency in PKR:" + pkr)
+var num = 3 + 5;
+var num1 = num * 10;
+var num2 = num1 / 2;
+document.write ("<br>" + "ANSWER 10:" + " " + num2)
+var currentYear = 2023;
+var birthYear = 2003;
+var age = currentYear - birthYear
+document.write ("<br>" + "ANSWER 11:" + "Your age is" + " " + age)
+var favouriteStore = "Chocolate chip"
+var currentAge = 15;
+var maximumAge = 65;
+var EstimatedAmount = 3;
+var total = maximumAge - currentAge;
+var total2 = total * EstimatedAmount
+document.write ("<br>" + "ANSWER 12:" + "You will need" + " " + total2 + " " + favouriteStore + " " + "to last you until the ripe old age is" + " " + maximumAge )
+
